b96480477e596e0a2fe972a80ad982b81a313e9c
ramiazmi <rami.azmi@gmail.com>
2019-11-08 23:43:11 +0200

    Update create_employee.py

--- Day-13/create_employee.py
+++ Day-13/create_employee.py
@@ -2,18 +2,17 @@ import pandas as pd
 from flask import Flask, request
 import json
 
-
 app = Flask(__name__)
 
-# Read employees data from a csv file when server starts
-employees_df = pd.read_csv('data/employees.csv')
+data_path = '../../'  # You can choose a more specific path
 
 
 @app.route('/api/employee/create', methods=['POST'])
 def list_employees_by_department():
     req_body = request.json
-    employee = json.dumps(dict(req_body))
-    return '{"employee":' + str(employee) + ', "Status": "A new employee was created successfully"}', 201
+    employee = dict(req_body)
+    pd.DataFrame(employee, index=[0]).to_csv(data_path + 'employees.csv', mode='a', header=False, index=False)
+    return '{"employee":' + str(json.dumps(employee)) + ', "Status": "A new employee was created successfully"}', 201
 
 
 if __name__ == '__main__':
