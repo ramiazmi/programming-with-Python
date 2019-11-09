a5ecae95a00745d620bf23c89fa5d294063434f2
ramiazmi <rami.azmi@gmail.com>
2019-11-09 17:28:06 +0200

    Update create_employee.py

--- Day-13/create_employee.py
+++ Day-13/create_employee.py
@@ -9,7 +9,7 @@ data_path = '../../'  # You can choose a more specific path
 
 @app.route('/api/employees', methods=['POST'])
 def list_employees_by_department():
-    req_body = request.json
+    req_body = request.data
     employee = dict(req_body)
     pd.DataFrame(employee, index=[0]).to_csv(data_path + 'employees.csv', mode='a', header=False, index=False)
     return '{"employee":' + str(json.dumps(employee)) + ', "Status": "A new employee was created successfully"}', 201
